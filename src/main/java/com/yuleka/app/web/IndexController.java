31060030b2576f5ccab12e404458b7714e74d4a9
dogyu <dydqud@gmail.com>
2020-04-13 14:56:04 +0900

    index clear

--- src/main/java/com/yuleka/app/web/IndexController.java
+++ src/main/java/com/yuleka/app/web/IndexController.java
@@ -1,13 +1,15 @@
 package com.yuleka.app.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("projectVer", "v1.0");
         return "index";
     }
 }
