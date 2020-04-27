024f7a0e6dc2cc0f742fb90480965782bf02fc35
yeolo03 <yeolo03@naver.com>
2020-04-27 15:46:00 +0900

    react test

---
+++ src/main/resources/static/precache-manifest.d5726a303d01fefbb00a9708edde5568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d97e958a1f2c6b7239bffb923f9624e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "266143449b8f6f52e678",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e775f1d6f9cfddff33a6",
+    "url": "/static/js/2.353916cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.353916cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266143449b8f6f52e678",
+    "url": "/static/js/main.98507b47.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  }
+]);
