f57befb08377af53c79168bd4a69933031717327
dogyu <dydqud@gmail.com>
2020-06-05 15:45:29 +0900

    chat 1.0

---
+++ src/main/java/com/yuleka/app/chat/security/model/LoginInfo.java
@@ -0,0 +1,15 @@
+package com.yuleka.app.chat.security.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class LoginInfo {
+    private String name;
+    private String token;
+    @Builder
+    public LoginInfo(String name, String token) {
+        this.name = name;
+        this.token = token;
+    }
+}
